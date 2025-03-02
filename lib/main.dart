d52c00bacde402b48beba9d6e8680a8ded171f1f
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-03-02 12:46:22 -0500

    [add] RouterGenerator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_bases/ui/pages/counter_page.dart';
-import 'package:flutter_web_bases/ui/pages/counter_provider_page.dart';
+import 'package:flutter_web_bases/router/router_generator.dart';
+// import 'package:flutter_web_bases/ui/pages/counter_page.dart';
+// import 'package:flutter_web_bases/ui/pages/counter_provider_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,12 +18,17 @@ class MyApp extends StatelessWidget {
       // routes: {
       //   'stateful': (BuildContext context) => CounterPage()
       // },
-      initialRoute: '/stateful',
-      routes: {
-        /* si no se está usando el BuildContext context se puede usar como "_" para hacer referencia a que no se está usando, pero en este caso lo vamos a dejar porque no nos causa mayor problema si se coloca o no */
-        '/stateful': (BuildContext context) => CounterPage(),
-        '/provider': (BuildContext context) => CounterProviderPage(),
-      },
+      initialRoute: '/stateful', // esta ruta inicial es opcional colocarla
+      // routes: {
+      //   /* si no se está usando el BuildContext context se puede usar como "_" para hacer referencia a que no se está usando, pero en este caso lo vamos a dejar porque no nos causa mayor problema si se coloca o no */
+      //   '/stateful': (BuildContext context) => CounterPage(),
+      //   '/provider': (BuildContext context) => CounterProviderPage(),
+      // },
+
+      /* aquí se está haciendo de forma separada en una función aparte pero es lo mismo, es decir, espera los mismos parámetros pero se está haciendo de esta forma para modularizar las cosas y tener todo más ordenado y hacer la separación de responsabilidades. Colocando esta generación de rutas entonces ya no es necesario colocar las rutas que estaban arriba */
+      /* se puede colocar la función completa pero darse cuenta que como el parámetro que se envía es igual al argumento que se recibe entonces se puede obviar colocar eso y solo colocar la referencia de la función tal cual se haría en JavaScript usando React por ejemplo */
+      // onGenerateRoute: (settings) => RouterGenerator.generateRoute(settings),
+      onGenerateRoute: RouterGenerator.generateRoute,
     );
   }
 }
---
+++ lib/router/router_generator.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_bases/ui/pages/counter_page.dart';
+import 'package:flutter_web_bases/ui/pages/counter_provider_page.dart';
+import 'package:flutter_web_bases/ui/pages/page_404.dart';
+
+/* este archivo es una clase que va a trabajar de la creación de todas las rutas de manera automática de la aplicación */
+
+class RouterGenerator {
+  /* como no queremos estar creando nuevas instancias, lo vamos a estar trabajando todo de maneja estática */
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    /* si no se está usando el BuildContext context se puede usar como "_" para hacer referencia a que no se está usando */
+    /* por defecto siempre se estará generando el CounterPage, es decir, cualquier ruta pasará por este código y devolverá el CounterPage pero ahora, lo que se necesita es que basado en el nombre de la ruta se necesita generar una pantalla o mostrar algún componente, etc */
+    // return MaterialPageRoute(builder: (BuildContext context) => CounterPage());
+    // return MaterialPageRoute(builder: (_) => CounterPage());
+
+    // print("***** RouterGenerator *****");
+    // print(RouteSettings);
+    // print(settings);
+
+    /* en los settings.name están los nombres de las rutas que se quiere navegar */
+    switch (settings.name) {
+      case "/stateful":
+        return MaterialPageRoute(builder: (_) => CounterPage());
+
+      case "/provider":
+        return MaterialPageRoute(builder: (_) => CounterProviderPage());
+
+      default:
+        return MaterialPageRoute(builder: (_) => Page404());
+    }
+  }
+}

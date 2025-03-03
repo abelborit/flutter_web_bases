303c4153386954edbe5abd9022862c0a0f7e30fa
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-03-03 14:06:13 -0500

    [add] CounterProvider and implementation

---
+++ lib/providers/counter_provider.dart
@@ -0,0 +1,27 @@
+/* los providers son una clase que le va permitir a Flutter redibujarse o renderizarse de nuevo cuando se les notifique a los listeners. Eso es muy facil, por eso los providers son uno de los gestores de estado más utilizados, el más usado es el GetX (https://pub.dev/packages/get) */
+
+import 'package:flutter/material.dart';
+
+class CounterProvider extends ChangeNotifier {
+  int _counter = 15;
+
+  /* FORMA 1 */
+  // get counter {
+  //   return _counter;
+  // }
+
+  /* FORMA 2 */
+  get counter => _counter;
+
+  /* estos valores serán privados entonces por eso se tiene que exponer la funcionalidad o la forma para que _counter pueda cambiar */
+  void increment() {
+    _counter++;
+    /* este "notifyListeners" va a notificar a todos los widgets que estén escuchando los cambios de este CounterProvider para que así se puedan a volver a re-renderizar con la información actualizada */
+    notifyListeners();
+  }
+
+  void decrement() {
+    _counter--;
+    notifyListeners();
+  }
+}
--- lib/ui/pages/counter_provider_page.dart
+++ lib/ui/pages/counter_provider_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_bases/providers/counter_provider.dart';
 import 'package:flutter_web_bases/ui/shared/custom_app_menu.dart';
 import 'package:flutter_web_bases/ui/shared/custom_flat_button.dart';
+import 'package:provider/provider.dart';
 
 /* cuando se trabaja con un provider o algún gestor de estado, usualmente no hace falta que sea un "StatefulWidget" porque con se un solo "StatelessWidget" nos va a funcionar */
 class CounterProviderPage extends StatelessWidget {
@@ -8,6 +10,20 @@ class CounterProviderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => CounterProvider(),
+      child: _CounterProviderPageBody(),
+    );
+  }
+}
+
+/* se extrae a una clase separada para poder trabajarlo de mejor forma con los providers pero no siempre se utilizará esta forma de extraer, algunas veces puede ser de forma directa o de alguna otra forma según el caso */
+class _CounterProviderPageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /* hacer referencia al provider que se quiere usar implementando el Provider.of<instancia_de_nuestro_provider>(context) */
+    final counterProvider = Provider.of<CounterProvider>(context);
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -24,7 +40,7 @@ class CounterProviderPage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(
-                "Contador: XXX",
+                "Contador: ${counterProvider.counter}", // se coloca ${.....} porque se va a acceder a una propiedad
                 style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
               ),
             ),
@@ -36,7 +52,7 @@ class CounterProviderPage extends StatelessWidget {
               CustomFlatButton(
                 text: "Incrementar",
                 /* FORMA 1 */
-                onPressed: () {},
+                onPressed: () => counterProvider.increment(),
                 /* FORMA 2 */
                 // onPressed: () {
                 //   setState(() {
@@ -48,7 +64,7 @@ class CounterProviderPage extends StatelessWidget {
                 text: "Decrementar",
                 color: Colors.red,
                 /* FORMA 1 */
-                onPressed: () {},
+                onPressed: () => counterProvider.decrement(),
                 /* FORMA 2 */
                 // onPressed: () {
                 //   setState(() {
